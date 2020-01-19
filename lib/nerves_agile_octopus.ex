e9d42029c36ae864908864c0cb9408c8cf0cee64
Ben Smith <ben@10consulting.com>
2020-01-19 10:53:02 +0000

    Initial commit

---
+++ lib/nerves_agile_octopus.ex
@@ -0,0 +1,18 @@
+defmodule NervesAgileOctopus do
+  @moduledoc """
+  Documentation for NervesAgileOctopus.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> NervesAgileOctopus.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/nerves_agile_octopus_test.exs
@@ -0,0 +1,8 @@
+defmodule NervesAgileOctopusTest do
+  use ExUnit.Case
+  doctest NervesAgileOctopus
+
+  test "greets the world" do
+    assert NervesAgileOctopus.hello() == :world
+  end
+end
